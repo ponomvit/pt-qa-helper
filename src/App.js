4e6da44c859befedfc7048e473ad297eff10dd1e
Vitaliy.Ponomarenko <Vitaliy.Ponomarenko@playtech.com>
2017-12-27 15:02:43 +0200

    vzhuh

--- src/App.js
+++ src/App.js
@@ -20,7 +20,6 @@ class App extends Component {
         this.onMessageListener();
     }
 
-
     onMessageListener() {
         chrome.runtime.onMessage.addListener((message, sender, response) => {
             console.log(message);
